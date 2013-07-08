1d45b22371b860c753a8b49878db6bab210d3fa9
Charles FD <cfreiden@yahoo.fr>
2013-07-08 23:24:37 +0200

    Add a parameter to manage nginx listening port

--- attributes/default.rb
+++ attributes/default.rb
@@ -87,3 +87,5 @@ default['nginx']['types_hash_bucket_size'] = 64
 
 default['nginx']['proxy_read_timeout'] = nil
 default['nginx']['client_max_body_size'] = nil
+
+default['nginx']['listening_port'] = 80
