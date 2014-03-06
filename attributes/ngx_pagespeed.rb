eb9174961ac3d8ec8f02766d0a206ca63b36726b
Charles FD <charles@ciblo.net>
2014-03-06 11:21:23 +0100

    Update pagespeed version

--- attributes/ngx_pagespeed.rb
+++ attributes/ngx_pagespeed.rb
@@ -19,12 +19,12 @@
 # limitations under the License.
 #
 
-default['nginx']['ngx_pagespeed']['version']  = "release-1.5.27.3-beta"
+default['nginx']['ngx_pagespeed']['version']  = "release-1.7.30.3-beta"
 default['nginx']['ngx_pagespeed']['url']      = "https://github.com/pagespeed/ngx_pagespeed/archive/#{node['nginx']['ngx_pagespeed']['version']}.tar.gz"
 default['nginx']['ngx_pagespeed']['src']['cookbook'] = "nginx"
 default['nginx']['ngx_pagespeed']['src']['file'] = nil
 
-default['nginx']['ngx_pagespeed']['psol']['version']  = "1.5.27.3"
+default['nginx']['ngx_pagespeed']['psol']['version']  = "1.7.30.3"
 default['nginx']['ngx_pagespeed']['psol']['url'] = "https://dl.google.com/dl/page-speed/psol/#{node['nginx']['ngx_pagespeed']['psol']['version']}.tar.gz"
 default['nginx']['ngx_pagespeed']['psol']['src']['cookbook'] = "nginx"
 default['nginx']['ngx_pagespeed']['psol']['src']['file'] = nil
